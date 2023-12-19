968d0bab84204f61d480255a67320588c16066cf
Eric Maranga <ekirira22@gmail.com>
2023-12-19 13:36:58 +0300

    completed tests

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const employee = {
+    name : "Eric Maranga",
+    streetAddress : "Fathers Hub, 6th Floor A02"
+}
+
+// Test 1. A function that non-destructively updates employee object. Receives 3 values. (Obj, Key, Value)
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const copyEmployee = {...obj}
+    copyEmployee[key] = value
+    return copyEmployee
+}
+
+//Test 2. A function that now destructively update the original employee object
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+    return obj
+}
+
+//Test 3. A function that non-destructively deletes a key from the original object
+function deleteFromEmployeeByKey(obj, key){
+    const copyEmployee = {...obj}
+    delete copyEmployee[key]
+    return copyEmployee
+}
+
+//Test 4. A function that destructively deletes a key from the original object
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key]
+    return obj
+}
